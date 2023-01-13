e42305f173ecfec685cd571c72a8f0bd75a0b14e
Matei Oltean <olteanmat@gmail.com>
2023-01-13 10:17:33 +0200

    Initial commit

---
+++ app/mini_calc.py
@@ -0,0 +1,21 @@
+class MiniCalc:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def adunare(self):
+        pass
+        #return self.a + self.b
+        return sum([self.a, self.b])
+
+    def scadere(self):
+        pass
+        return self.a - self.b
+
+    def inmultire(self):
+        pass
+        return self.a * self.b
+
+    def impartire(self):
+        pass
+        return self.a / self.b
---
+++ cmd
@@ -0,0 +1,12 @@
+pip install pytest
+
+pytest .\folder_name\
+pytest .\folder_name\file_name.py
+
+
+
+https://thinking-tester-contact-list.herokuapp.com/addUser
+
+https://www.postman.com/ladyab/workspace/my-workspace/request/800617-ee39d0f2-59db-4cf3-868c-e7db1b6ef29a
+
+https://thinking-tester-contact-list.herokuapp.com/addUser
